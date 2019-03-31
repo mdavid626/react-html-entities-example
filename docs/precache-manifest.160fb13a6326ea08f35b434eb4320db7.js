889cfec0ca0f02749a993535272118c7ae6708cf
Dávid Molnár <mdavid626@gmail.com>
2019-03-31 08:14:29 +0200

    Add docs page

---
+++ docs/precache-manifest.160fb13a6326ea08f35b434eb4320db7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "95f1c400e1a55e4a1fcb",
+    "url": "/static/js/main.5975fd27.chunk.js"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "95f1c400e1a55e4a1fcb",
+    "url": "/static/css/main.88aaee42.chunk.css"
+  },
+  {
+    "revision": "24c804b074f0007366b6b09fe0ee030a",
+    "url": "/index.html"
+  }
+];
